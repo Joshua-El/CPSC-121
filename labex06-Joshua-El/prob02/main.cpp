e8d938680af08d69573274e3a3d2518db530d3a8
Joshua Elmer <54691663+Joshua-El@users.noreply.github.com>
2022-02-10 21:35:29 -0800

    Update main.cpp

--- labex06-Joshua-El/prob02/main.cpp
+++ labex06-Joshua-El/prob02/main.cpp
@@ -5,7 +5,7 @@
 int main() {
   std::string header;
   std::string text;
-  std::cout << "Please enter header text: ";
+  std::cout << "Please enter header text: "; 
   std::getline(std::cin, text);
 
   if (within_width(text, 80)) {
